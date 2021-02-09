e020721356ee34eae098efde54c2dc5ee4b290e5
jungheejung <heejung.jung@colorado.edu>
2021-02-09 14:50:30 -0500

    update run scheme

--- scripts/RUN_narratives_final.m
+++ scripts/RUN_narratives_final.m
@@ -12,7 +12,20 @@ biopac = input(prompt);
 
 fmri = 0;
 
-narratives_final(sub, srun, biopac, fmri)
+run_t1 = strcat( "narratives(", num2str(sub), "',", num2str(1), "',", num2str(biopac), "',", num2str(fmri), ")");
+run_t2 = strcat( "narratives(", num2str(sub), "',", num2str(2), "',", num2str(biopac), "',", num2str(fmri), ")");
+run_t3 = strcat( "narratives(", num2str(sub), "',", num2str(3), "',", num2str(biopac), "',", num2str(fmri), ")");
+run_t4 = strcat( "narratives(", num2str(sub), "',", num2str(4), "',", num2str(biopac), "',", num2str(fmri), ")");
+
+if srun == 1
+eval(run_t1);eval(run_t2);eval(run_t3);eval(run_t4);
+elseif srun == 2
+eval(run_t2);eval(run_t3);eval(run_t4);
+elseif srun == 3
+eval(run_t3);eval(run_t4);
+elseif srun == 4
+eval(run_t4);
+end
 
 
 
